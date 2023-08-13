49f6a640f53451ec2d70e4f9a7789ed015196501
Toan <tpkhuong@gmail.com>
2023-08-13 04:03:42 -0400

    enjoyed the effort/process of building algorithm chip drop animation and player chip placement mobile, tablet and desktop connect four dir.

--- advanced/nextJsProjects/connectFourGame/src/app/components/client/ConnectFourBoard/connectFourBoardHelpers.js
+++ advanced/nextJsProjects/connectFourGame/src/app/components/client/ConnectFourBoard/connectFourBoardHelpers.js
@@ -12,6 +12,7 @@ function rowCounter() {
   const objOfMethods = {
     first: () => {
       // there is no seventh row, return
+      console.log(objOfCounter);
       if (objOfCounter.columnOne == 7) return;
       // add 1 to column counter
       objOfCounter.columnOne += 1;
@@ -22,6 +23,65 @@ function rowCounter() {
         "data-droppedposition",
         `${objOfCounter.columnOne}`
       );
+      // show player chip
+      // row 6
+      if (objOfCounter.columnOne == 6) {
+        setTimeout(() => {
+          document
+            .getElementById("row-6")
+            .firstElementChild.setAttribute("data-playerchip", "two");
+        }, 100);
+      }
+
+      // row 5
+      if (objOfCounter.columnOne == 5) {
+        const time = window.innerWidth <= 378 ? 1260 : 1310;
+        setTimeout(() => {
+          document
+            .getElementById("row-5")
+            .firstElementChild.setAttribute("data-playerchip", "two");
+        }, time);
+      }
+
+      // row 4
+      if (objOfCounter.columnOne == 4) {
+        const time = window.innerWidth <= 378 ? 1290 : 1340;
+        setTimeout(() => {
+          document
+            .getElementById("row-4")
+            .firstElementChild.setAttribute("data-playerchip", "two");
+        }, time);
+      }
+
+      // row 3
+      if (objOfCounter.columnOne == 3) {
+        const time = window.innerWidth <= 378 ? 1320 : 1370;
+        setTimeout(() => {
+          document
+            .getElementById("row-3")
+            .firstElementChild.setAttribute("data-playerchip", "two");
+        }, time);
+      }
+
+      // row 2
+      if (objOfCounter.columnOne == 2) {
+        const time = window.innerWidth <= 378 ? 1350 : 1400;
+        setTimeout(() => {
+          document
+            .getElementById("row-2")
+            .firstElementChild.setAttribute("data-playerchip", "two");
+        }, time);
+      }
+
+      // row 1
+      if (objOfCounter.columnOne == 1) {
+        const time = window.innerWidth <= 378 ? 1370 : 1420;
+        setTimeout(() => {
+          document
+            .getElementById("row-1")
+            .firstElementChild.setAttribute("data-playerchip", "two");
+        }, time);
+      }
     },
     second: () => {
       // there is no seventh row, return
