4c99841aef9997111cd7e80485935e0bd8634b7a
Toan <tpkhuong@gmail.com>
2023-08-18 04:04:07 -0400

    enjoyed the effort/process of building algorithm to get attr to access chip node in array matrix column one connect four dir.

--- advanced/nextJsProjects/connectFourGame/src/app/components/client/ConnectFourBoard/connectFourBoardHelpers.js
+++ advanced/nextJsProjects/connectFourGame/src/app/components/client/ConnectFourBoard/connectFourBoardHelpers.js
@@ -2,22 +2,22 @@ function rowCounter() {
   const arrayOfChipNodes = [
     // row 6 - 6
     // index 0
-    [],
+    ["6-1", "6-2", "6-3", "6-4", "6-5", "6-6", "6-7"],
     // row 5 - 4
     // index 1
-    [],
+    ["5-1", "5-2", "5-3", "5-4", "5-5", "5-6", "5-7"],
     // row 4 - 2
     // index 2
-    [],
+    ["4-1", "4-2", "4-3", "4-4", "4-5", "5-6", "6-7"],
     // row 3
     // index 3
-    [],
+    ["3-1", "3-2", "3-3", "3-4", "3-5", "3-6", "3-7"],
     // row 2 + 2
     // index 4
-    [],
+    ["2-1", "2-2", "2-3", "2-4", "2-5", "2-6", "2-7"],
     // row 1 + 4
     // index 5
-    [],
+    ["1-1", "1-2", "1-3", "1-4", "1-5", "1-6", "1-7"],
   ];
   const objOfCounter = {
     columnOne: 0,
@@ -62,11 +62,11 @@ function rowCounter() {
       // select first column
       const firstColumn = document.getElementById("animation-column-first");
       // access position in arrayOfChipNodes here
-      console.log(
-        document.querySelector(
-          `#row-${objOfCounter.columnOne} [data-chipselector='${objOfCounter.columnOne}-${first}']`
-        )
-      );
+      // console.log(
+      //   document.querySelector(
+      //     `#row-${objOfCounter.columnOne} [data-chipselector='${objOfCounter.columnOne}-${first}']`
+      //   )
+      // );
 
       const chipNodeElement = document.querySelector(
         `#row-${objOfCounter.columnOne} [data-chipselector='${objOfCounter.columnOne}-${first}']`
@@ -81,7 +81,17 @@ function rowCounter() {
       // show player chip
       // row 6
       if (objOfCounter.columnOne == 6) {
+        console.log(chipNodeElement, "row 6");
+        const [row, column] = chipNodeElement
+          .getAttribute("data-chipselector")
+          .split("-");
+        console.log(row, "row");
+        console.log(column, "column");
         // row 6 - 6
+        console.log(
+          arrayOfChipNodes[Number(row) - 6][Number(column)],
+          "array node"
+        );
         setTimeout(() => {
           document
             .getElementById("row-6")
@@ -91,7 +101,17 @@ function rowCounter() {
 
       // row 5
       if (objOfCounter.columnOne == 5) {
+        console.log(chipNodeElement, "row 5");
+        const [row, column] = chipNodeElement
+          .getAttribute("data-chipselector")
+          .split("-");
+        console.log(row, "row");
+        console.log(column, "column");
         // row 5 - 4
+        console.log(
+          arrayOfChipNodes[Number(row) - 4][Number(column)],
+          "array node"
+        );
         const time = window.innerWidth <= 378 ? 1260 : 1310;
         setTimeout(() => {
           document
@@ -102,7 +122,17 @@ function rowCounter() {
 
       // row 4
       if (objOfCounter.columnOne == 4) {
+        console.log(chipNodeElement, "row 4");
+        const [row, column] = chipNodeElement
+          .getAttribute("data-chipselector")
+          .split("-");
+        console.log(row, "row");
+        console.log(column, "column");
         // row 4 - 2
+        console.log(
+          arrayOfChipNodes[Number(row) - 2][Number(column)],
+          "array node"
+        );
         const time = window.innerWidth <= 378 ? 1290 : 1340;
         setTimeout(() => {
           document
@@ -113,7 +143,17 @@ function rowCounter() {
 
       // row 3
       if (objOfCounter.columnOne == 3) {
+        console.log(chipNodeElement, "row 3");
+        const [row, column] = chipNodeElement
+          .getAttribute("data-chipselector")
+          .split("-");
+        console.log(row, "row");
+        console.log(column, "column");
         // row 3
+        console.log(
+          arrayOfChipNodes[Number(row)][Number(column)],
+          "array node"
+        );
         const time = window.innerWidth <= 378 ? 1320 : 1370;
         setTimeout(() => {
           document
@@ -124,7 +164,17 @@ function rowCounter() {
 
       // row 2
       if (objOfCounter.columnOne == 2) {
+        console.log(chipNodeElement, "row 2");
+        const [row, column] = chipNodeElement
+          .getAttribute("data-chipselector")
+          .split("-");
+        console.log(row, "row");
+        console.log(column, "column");
         // row 2 + 2
+        console.log(
+          arrayOfChipNodes[Number(row) + 2][Number(column)],
+          "array node"
+        );
         const time = window.innerWidth <= 378 ? 1350 : 1400;
         setTimeout(() => {
           document
@@ -135,7 +185,17 @@ function rowCounter() {
 
       // row 1
       if (objOfCounter.columnOne == 1) {
+        console.log(chipNodeElement, "row 1");
+        const [row, column] = chipNodeElement
+          .getAttribute("data-chipselector")
+          .split("-");
+        console.log(row, "row");
+        console.log(column, "column");
         // row 1 + 4
+        console.log(
+          arrayOfChipNodes[Number(row) + 4][Number(column)],
+          "array node"
+        );
         const time = window.innerWidth <= 378 ? 1370 : 1420;
         setTimeout(() => {
           document
@@ -147,11 +207,16 @@ function rowCounter() {
     second: ({ second }) => {
       console.log(second);
       // there is no seventh row, return
-      if (objOfCounter.columnOne == 7) return;
+      if (objOfCounter.columnTwo == 7) return;
       // add 1 to column counter
       objOfCounter.columnTwo += 1;
       // select second column
       const secondColumn = document.getElementById("animation-column-second");
+
+      const chipNodeElement = document.querySelector(
+        `#row-${objOfCounter.columnTwo} [data-chipselector='${objOfCounter.columnTwo}-${second}']`
+      );
+
       // set value of data-droppedposition attr based on current counter
       secondColumn.setAttribute(
         "data-droppedposition",
@@ -160,26 +225,33 @@ function rowCounter() {
     },
     third: ({ third }) => {
       console.log(third);
+      // there is no seventh row, return
+      if (objOfCounter.columnThree == 7) return;
       objOfCounter.columnThree += 1;
     },
     fourth: ({ fourth }) => {
       console.log(fourth);
-
+      // there is no seventh row, return
+      if (objOfCounter.columnFour == 7) return;
       objOfCounter.columnFour += 1;
     },
     fifth: ({ fifth }) => {
       console.log(fifth);
+      // there is no seventh row, return
+      if (objOfCounter.columnFive == 7) return;
       objOfCounter.columnFive += 1;
     },
     sixth: ({ sixth }) => {
       console.log(sixth);
-
+      // there is no seventh row, return
+      if (objOfCounter.columnSix == 7) return;
       objOfCounter.columnSix += 1;
     },
     seventh: ({ seventh }) => {
       console.log(seventh);
-
-      objOfCounter.columnOne += 1;
+      // there is no seventh row, return
+      if (objOfCounter.columnSeven == 7) return;
+      objOfCounter.columnSeven += 1;
     },
   };
 
